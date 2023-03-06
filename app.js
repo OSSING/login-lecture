cc7d8a08e690852fc73942de0052374cadbc21d8
Gil Ohsung <kos1090291@gmail.com>
2023-03-06 16:24:10 +0900

    깃 저장소 초기화

---
+++ app.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// 모듈
+const express = require("express");
+const app = express();
+
+const home = require("./routes/home");
+
+// 앱 세팅
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.use("/", home); // use -> 미들웨어 등록 메소드
+
+module.exports = app;
