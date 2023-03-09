0e7e8521d7b556ffb330d1adb5f577de38f81ac6
Gil Ohsung <kos1090291@gmail.com>
2023-03-09 13:28:59 +0900

    DOM을 이용한 HTML객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,14 @@
 "use strict";
 
-console.log("hello");
-console.log("bye");
+const id  = document.querySelector("#id"),  // # -> id에 부여된 id를 가져옴
+    psword = document.querySelector("#password"),
+    loginBtn = document.querySelector("button"); // 질의 선택자
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.value,
+        psword: id.value
+    };
+}
