28029f6cd97c63e31008f936877a5d4d10e7e2e3
Gil Ohsung <kos1090291@gmail.com>
2023-03-11 13:51:49 +0900

    UserStorage 모델 구현

---
+++ app/src/models/UserStorage.js
@@ -0,0 +1,24 @@
+"use strict"
+
+const { reduce } = require("async");
+
+class UserStorage {      // #처리로 public 변수를 private 변수로 만들어 외부에서 접근 불가
+    static #users = {    // static 으로 정적 변수를 만들어 줌으로써 클래스 자체에서 변수에 접근 가능
+        id: ["ohsing", "길개발", "길인턴"],
+        psword: ["1234", "1234", "123456"],
+        name: ["길오성", "이예린", "이종명"],
+    };
+
+    static getUsers(...fields) {     // static으로 클래스에서 변수에 직접 접근 가능
+        const users = this.#users;
+        const newUsers = fields.reduce((newUsers, field) => {  // fields 배열의 초기 값이 newUsers에 들어가고 다음 변수들은 fields에 들어감
+            if (users.hasOwnProperty(field)) {
+                newUsers[field] = users[field];
+            }
+            return newUsers;
+        }, {});
+        return newUsers; // 은닉화된 private 변수를 반환
+    }
+}
+
+module.exports = UserStorage;   // 해당 클래스를 밖에서 사용할 수 있도록 함.
