167f221d6c674a48600a5a0b5eef310539f3a012
Gil Ohsung <kos1090291@gmail.com>
2023-03-28 23:28:08 +0900

    회원가입 요청 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -1,33 +1,37 @@
 "use strict";   // HTML과 연결된 JS 파일 부분 (front 영역)
 
 const id  = document.querySelector("#id"),  // # -> id에 부여된 id를 가져옴
+    name = document.querySelector("#name"),
     psword = document.querySelector("#psword"),
-    loginBtn = document.querySelector("#button"); // 질의 선택자
+    confirmPsword = document.querySelector("#confirm-psword"),
+    registerBtn = document.querySelector("#button"); // 질의 선택자
+    
+registerBtn.addEventListener("click", register);
 
-loginBtn.addEventListener("click", login);
-
-function login() {
+function register() {
     const req = {
         id: id.value,
-        psword: psword.value
+        name: name.value,
+        psword: psword.value,
+        confirmPsword: confirmPsword.value,
     };
 
-    fetch("/login", {
+    fetch("/register", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
         },
-        body: JSON.stringify(req),
+        body: JSON.stringify(req),  // 회원가입 시 회원 정보를 body에 저장
     })
     .then((res) => res.json())          // res.json()의 반환 값은 Promise
     .then((res) => {
         if (res.success) {
-            location.href = "/";
+            location.href = "/login";
         } else {
             alert(res.msg);
         }
     })
     .catch((err) => {
-        console.error("로그인 중 에러 발생");
+        console.error("회원가입 중 에러 발생");
     });
 }
